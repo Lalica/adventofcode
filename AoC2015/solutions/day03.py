500c13802e25b58fa692f4ba6250f4427dea2c36
laura <lauratoric@gmail.com>
2018-10-31 21:08:37 +0100

    day 7 and refactor day 3

--- AoC2015/solutions/day03.py
+++ AoC2015/solutions/day03.py
@@ -1,31 +1,19 @@
-def move(arrow):
-    if arrow == '^':
-        return -10000
-    elif arrow == 'v':
-        return 10000
-    elif arrow == '<':
-        return -1
-    elif arrow == '>':
-        return 1
-
-
 with open("../inputs/03.txt") as f:
     input = f.read().strip()
 
-current, houses = 0, []
-houses.append(current)
+step = len(input)
+move = {'^': -step, 'v': step, '<': -1, '>': 1}
+current1, houses1, current2, houses2 = [0], set(), [0, 0], set()
+houses1.add(current1[0])
+houses2.add(current1[0])
 
 for i in range(len(input)):
-    current += move(input[i])
-    houses.append(current)
-print("Day 3 part 1: " + str(len(set(houses))))
+    # part 1
+    current1[0] += move[input[i]]
+    houses1.add(current1[0])
+    # part 2
+    current2[i % 2] += move[input[i]]
+    houses2.add(current2[i % 2])
 
-currentS, currentR, houses = 0, 0, []
-houses.append(currentS)
-
-for i in range(0, len(input), 2):
-    currentS += move(input[i])
-    houses.append(currentS)
-    currentR += move(input[i+1])
-    houses.append(currentR)
-print("Day 3 part 2: " + str(len(set(houses))))
+print("Day 3 part 1: " + str(len(houses1)))
+print("Day 3 part 2: " + str(len(houses2)))
