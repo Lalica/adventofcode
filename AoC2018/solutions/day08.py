80ea955bceefb8b21cd0c381f8d4750519a2cda8
laura <lauratoric@gmail.com>
2018-12-08 11:44:40 +0100

    refactoring of day 8

--- AoC2018/solutions/day08.py
+++ AoC2018/solutions/day08.py
@@ -11,11 +11,10 @@ def tree2(head, part):
     for i in range(nodes[head+1]):
         indexes += children[nodes[head + 2 + i] - 1] if nodes[head + 2 + i] <= nodes[head] else 0
     nodes = nodes[:head] + nodes[head + 2 + nodes[head + 1]:]
-    if part == 1:
-        return meta + sum(children)
-    return indexes if indexes != 0 else meta
+
+    return meta + sum(children) if part == 1 else indexes if indexes != 0 else meta
 
 
-print("Dat 8 part 1: " + str(tree2(0, 1)))
+print("Day 8 part 1: " + str(tree2(0, 1)))
 nodes = [int(i) for i in open("../inputs/08.txt").read().strip().split()]
-print("Dat 8 part 2: " + str(tree2(0, 2)))
+print("Day 8 part 2: " + str(tree2(0, 2)))
