65bee26015fdaaf0cc25672a758281fe02faf3c9
Laura Toric <lauratoric@gmail.com>
2020-04-09 00:19:05 +0200

    updated output

--- AoC2015/solutions/day09.py
+++ AoC2015/solutions/day09.py
@@ -5,5 +5,5 @@ with open('../inputs/9.txt') as f:
     edges = {frozenset(x.split(' to ')): int(y) for x, y in map(lambda l: l.split(' = '), f)}
     cities = set(chain(*edges))
     all_sums = [sum(edges[frozenset((p[i], p[i+1]))] for i in range(len(p)-1)) for p in permutations(cities)]
-    print(min(all_sums))
-    print(max(all_sums))
+    print('Day 9 part 1: ' + str(min(all_sums)))
+    print('Day 9 part 2: ' + str(max(all_sums)))
