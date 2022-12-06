df01c6f79d1a199f2e5aeb861a3bdb20e5c74eca
Lalica <lauratoric@gmail.com>
2022-12-06 10:05:35 +0100

    fixed typo

--- AoC2022/solutions/day03.py
+++ AoC2022/solutions/day03.py
@@ -18,8 +18,8 @@ with open("../inputs/03.txt") as f:
 conpartments = [tuple(map(set, halves(r))) for r in rucksacks]
 shared_items = [next(iter(c1 & c2)) for c1, c2 in conpartments]
 part1 = sum(priority(item) for item in shared_items)
-print(f'Day 1 part 1: {part1}')
+print(f'Day 3 part 1: {part1}')
 
 badges = [next(iter(set.intersection(*map(set, g)))) for g in groups_of_3(rucksacks)]
 part2 = sum(priority(badge) for badge in badges)
-print(f'Day 1 part 2: {part2}')
+print(f'Day 3 part 2: {part2}')
--- AoC2022/solutions/day04.py
+++ AoC2022/solutions/day04.py
@@ -5,7 +5,7 @@ with open("../inputs/04.txt") as f:
     pairs = [list(map(int, re.split(",|-", line))) for line in f]
 
 part1 = sum((a >= x and b <= y) or (x >= a and y <= b) for a, b, x, y in pairs)
-print(f'Day 1 part 1: {part1}')
+print(f'Day 4 part 1: {part1}')
 
 part2 = len(pairs) - sum(b < x or y < a for a, b, x, y in pairs)
-print(f'Day 1 part 2: {part2}')
+print(f'Day 4 part 2: {part2}')
