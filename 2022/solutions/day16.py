17c725495bfa42056b98e3231272ee7d9bede231
Lalica <lauratoric@gmail.com>
2023-01-09 10:59:08 +0100

    added 2022 day 16

--- 2022/solutions/day16.py
+++ 2022/solutions/day16.py
@@ -1,84 +1,53 @@
-from itertools import product, combinations
+import re
 from collections import defaultdict
+from itertools import product, combinations
+from functools import cache
+
+
+@cache
+def open_valves(time, v, closed):
+    if time <= 0:
+        return 0
+
+    best_score = 0
+    for u in closed:
+        time_left = time - dist[v, u] - 1
+        score = pressures[u] * time_left + open_valves(time_left, u, closed - {u})
+        best_score = max(best_score, score)
+
+    return best_score
 
 
 def powerset(s):
     return [frozenset(c) for r in range(len(s) + 1) for c in combinations(s, r)]
 
 
-def shortest_paths(graph, vents, start):
-    nodes = graph.keys()
-    dist = defaultdict(lambda: float("inf"))
-
-    for s in graph:
-        for e in graph[s]:
-            dist[(s, e)] = 1
-    for s in nodes:
-        dist[(s, s)] = 0
-
-    for k in nodes:
-        for i in nodes:
-            for j in nodes:
-                if dist[(i, j)] > dist[(i, k)] + dist[(k, j)]:
-                    dist[(i, j)] = dist[(i, k)] + dist[(k, j)]
-
-    nodes = vents | {start}
-    return {s: [(e, dist[(s, e)]) for e in nodes if e != s] for s in nodes}
-
-
-
-def calculate_pressures(start_time, graph):
-    scores = dict()
-    subsets = powerset(set(graph.keys()))
-
-    for node in graph:
-        for opened in subsets:
-            scores[(0, node, opened)] = 0
-
-    for time in range(1, start_time + 1):
-        for node in graph:
-            for opened in subsets:
-                best_score = 0
-                for nbr, dist in graph[node]:
-                    time_left = time - dist
-                    if time_left >= 0:
-                        best_score = max(best_score, scores[(time_left, nbr, opened)])
-
-                    if nbr not in opened and time_left >= 1:
-                        score = pressures[nbr] * (time_left - 1) + \
-                                scores[(time_left - 1, nbr, opened | frozenset({nbr}))]
-                        best_score = max(best_score, score)
-
-                scores[(time, node, opened)] = best_score
-
-    return scores
-
-
-def combine(time, pos, scores, vents):
+def combine(time, pos, valves):
     return max(
-            scores[(time, pos, subset)] +
-            scores[(time, pos, vents - subset)]
-            for subset in powerset(vents))
+            open_valves(time, pos, subset) +
+            open_valves(time, pos, valves - subset)
+            for subset in powerset(valves))
 
 
 with open("../inputs/16.txt") as f:
-    graph = dict()
-    start = "AA"
-    pressures = {start: 0}
-    for line in f:
-        line = line.split()
-        node = line[1]
-        pressure = int(line[4].split("=")[1][:-1])
-        graph[node]= "".join(line[9:]).split(",")
-        if pressure:
-            pressures[node] = pressure
+    pressures = dict()
+    V = set()
+    dist = defaultdict(lambda: float("inf"))
 
-vents = frozenset(pressures.keys())
-graph = shortest_paths(graph, vents, start) 
-scores = calculate_pressures(30, graph)
+    r = r'Valve (\w+) .*=(\d*); .* valves? (.*)'
+    for v, pressure, nbrs in re.findall(r, f.read()):
+        V.add(v)
+        if pressure != '0':
+            pressures[v] = int(pressure)
+        for u in nbrs.split(', '):
+            dist[v, u] = 1
 
-part1 = scores[30, start, frozenset()]
+for k, i, j in product(V, V, V):
+    dist[i, j] = min(dist[i, j], dist[i, k] + dist[k, j])
+valves = frozenset(pressures.keys())
+
+part1 = open_valves(30, 'AA', valves)
 print(f'Day 16 part 1: {part1}')
 
-part2 = combine(26, start, scores, vents)
+part2 = combine(26, 'AA', valves)
 print(f'Day 16 part 2: {part2}')
